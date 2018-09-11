d3903bd3416a3b008ab2d5e10e4bc0b164746b66
Nirantak Raghav <me@nirantak.com>
2018-09-11 22:56:59 +0530

    Update code for Python37

--- Data_Mining/bayes.py
+++ Data_Mining/bayes.py
@@ -3,82 +3,79 @@ Use Bayesian Classification to solve the given problem.
 Dataset: bayes.csv
 """
 from functools import reduce
+from typing import Dict
 
 import pandas as pd
 
-df = pd.read_csv("bayes.csv", encoding="utf8")
-i = 0
-c_yes = 0
-c_no = 0
-X = {}
-pn_yes = []
-pn_no = []
 
-print("Dataset:")
-print(df)
+def main(dataset_path: str, X: Dict):
+    df = pd.read_csv(dataset_path, encoding="utf8")
+    i = 0
+    c_yes = 0
+    c_no = 0
 
-X["age"] = input("\nEnter class for age: ")
-X["income"] = input("Enter class for income: ")
-X["student"] = input("Enter class for student: ")
-X["credit_rating"] = input("Enter class for credit_rating: ")
+    pn_yes = []
+    pn_no = []
 
-for index, row in df.iterrows():
-    if row.buys_computer == "yes":
-        c_yes = c_yes + 1
-    else:
-        c_no = c_no + 1
-pc_yes = round(c_yes / 14, 3)
-pc_no = round(c_no / 14, 3)
+    print("Dataset:")
+    print(df)
 
-print(u"\nP(C\u1D62):")
-print("\tP(buys_computer = 'yes') = {}".format(pc_yes))
-print("\tP(buys_computer = 'no') = {}".format(pc_no))
-
-print(
-    "\nX = (age {}, income = {}, student = {}, credit_rating = {})".format(
-        X["age"], X["income"], X["student"], X["credit_rating"]
-    )
-)
-
-print(u"\nP(X|C\u1D62) for each class:")
-for key, value in X.items():
-    n_yes = 0
-    n_no = 0
     for index, row in df.iterrows():
-        if row.buys_computer == "yes" and row[key] == value:
-            n_yes = n_yes + 1
-        elif row.buys_computer == "no" and row[key] == value:
-            n_no = n_no + 1
-    pn_yes.append(round(n_yes / 9, 3))
-    pn_no.append(round(n_no / 5, 3))
+        if row.buys_computer == "yes":
+            c_yes += 1
+        else:
+            c_no += 1
+    pc_yes = round(c_yes / 14, 3)
+    pc_no = round(c_no / 14, 3)
 
-    print("\tP({} = {}|buys_computer = 'yes') = {}".format(key, value, pn_yes[i]))
-    print("\tP({} = {}|buys_computer = 'no') = {}".format(key, value, pn_no[i]))
-    i = i + 1
+    print("\nP(C\u1D62):")
+    print(f"\tP(buys_computer = 'yes') = {pc_yes}")
+    print(f"\tP(buys_computer = 'no') = {pc_no}")
 
-pxc_yes = round(reduce(lambda x, y: x * y, pn_yes, 1), 3)
-pxc_no = round(reduce(lambda x, y: x * y, pn_no, 1), 3)
-
-print(u"\nP(X|C\u1D62):")
-print("\tP(X|buys_computer = 'yes') = {}".format(pxc_yes))
-print("\tP(X|buys_computer = 'no') = {}".format(pxc_no))
-
-pcx_yes = pxc_yes * pc_yes
-pcx_no = pxc_no * pc_no
-print(u"\nP(C\u1D62|X) = P(X|C\u1D62)*P(C\u1D62):")
-print(
-    "\tP(X|buys_computer = 'yes') * P(buys_computer = 'yes') = {}".format(
-        round(pcx_yes, 3)
+    print(
+        f"\nX = (age {X['age']}, income = {X['income']}, student = {X['student']}, credit_rating = {X['credit_rating']}"
     )
-)
-print(
-    "\tP(X|buys_computer = 'no') * P(buys_computer = 'no') = {}".format(
-        round(pcx_no, 3)
-    )
-)
 
-print(
-    "\nThe given query is classified as: \n\t'buys_computer' = '{}'".format(
-        "yes" if pcx_yes > pcx_no else "no"
+    print("\nP(X|C\u1D62) for each class:")
+    for key, value in X.items():
+        n_yes = 0
+        n_no = 0
+        for index, row in df.iterrows():
+            if row.buys_computer == "yes" and row[key] == value:
+                n_yes += 1
+            elif row.buys_computer == "no" and row[key] == value:
+                n_no += 1
+        pn_yes.append(round(n_yes / 9, 3))
+        pn_no.append(round(n_no / 5, 3))
+
+        print(f"\tP({key} = {value}|buys_computer = 'yes') = {pn_yes[i]}")
+        print(f"\tP({key} = {value}|buys_computer = 'no') = {pn_no[i]}")
+        i += 1
+
+    pxc_yes = round(reduce(lambda x, y: x * y, pn_yes, 1), 3)
+    pxc_no = round(reduce(lambda x, y: x * y, pn_no, 1), 3)
+
+    print("\nP(X|C\u1D62):")
+    print(f"\tP(X|buys_computer = 'yes') = {pxc_yes}")
+    print(f"\tP(X|buys_computer = 'no') = {pxc_no}")
+
+    pcx_yes = pxc_yes * pc_yes
+    pcx_no = pxc_no * pc_no
+    print("\nP(C\u1D62|X) = P(X|C\u1D62)*P(C\u1D62):")
+    print(
+        f"\tP(X|buys_computer = 'yes') * P(buys_computer = 'yes') = {round(pcx_yes, 3)}"
     )
-)
+    print(f"\tP(X|buys_computer = 'no') * P(buys_computer = 'no') = {round(pcx_no, 3)}")
+
+    print(
+        f"\nThe given query is classified as: \n\t'buys_computer' = '{'yes' if pcx_yes > pcx_no else 'no'}'"
+    )
+
+
+if __name__ == "__main__":
+    X = {}
+    X["age"] = input("\nEnter class for age: ")
+    X["income"] = input("Enter class for income: ")
+    X["student"] = input("Enter class for student: ")
+    X["credit_rating"] = input("Enter class for credit_rating: ")
+    main("bayes.csv", X)
--- Data_Mining/kmeans.py
+++ Data_Mining/kmeans.py
@@ -1,46 +1,51 @@
 """
 Find clusters for input dataset using KMeans Algorithm.
 """
+from typing import List
 
-x = input("Enter the dataset: ")
-data = list(map(int, x.split()))
 
-x = input("Enter 2 mean values(m1 and m2): ")
-m1, m2 = map(float, x.split())
+def main(data: List[int], m1: float, m2: float):
+    print(f"\n\tDataset: {data}")
+    print(f"\tMeans: m1={m1}, m2={m2} \n")
 
-print("\n\tDataset: {}".format(data))
-print("\tMeans: m1={}, m2={} \n".format(m1, m2))
+    while True:
+        k1 = []
+        k2 = []
+        c1 = c2 = sum1 = sum2 = 0
+        pm1, pm2 = m1, m2
 
-while True:
-    k1 = []
-    k2 = []
-    c1 = c2 = sum1 = sum2 = 0
-    pm1, pm2 = m1, m2
+        for i in data:
+            if abs(m1 - i) < abs(m2 - i):
+                k1.append(i)
+                c1 += 1
+            else:
+                k2.append(i)
+                c2 += 1
 
-    for i in data:
-        if abs(m1 - i) < abs(m2 - i):
-            k1.append(i)
-            c1 += 1
-        else:
-            k2.append(i)
-            c2 += 1
+        print(f"\tk1: {k1}")
+        print(f"\tk2: {k2}")
 
-    print("\tk1: {}".format(k1))
-    print("\tk2: {}".format(k2))
+        for i in range(c1):
+            sum1 += k1[i]
+        m1 = sum1 / c1
 
-    for i in range(c1):
-        sum1 = sum1 + k1[i]
-    m1 = sum1 / c1
+        for i in range(c2):
+            sum2 += k2[i]
+        m2 = sum2 / c2
 
-    for i in range(c2):
-        sum2 = sum2 + k2[i]
-    m2 = sum2 / c2
+        print(f"\tThe new means for k1 and k2 are: m1={m1}, m2={m2} \n")
+        if pm1 == m1 and pm2 == m2:
+            break
 
-    print("\tThe new means for k1 and k2 are: m1={}, m2={} \n".format(m1, m2))
-    if pm1 == m1 and pm2 == m2:
-        break
+    print(
+        "Since new mean is equal to mean obtained in the last step, execution is stopped here."
+    )
 
-print(
-    "Since new mean is equal to mean obtained in the last step,\
-execution is stopped here."
-)
+
+if __name__ == "__main__":
+    x = input("Enter the dataset: ")
+    data = list(map(int, x.split()))
+
+    x = input("Enter 2 mean values(m1 and m2): ")
+    m1, m2 = map(float, x.split())
+    main(data, m1, m2)
